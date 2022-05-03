13468dd266b2df1c9dba29cfec8e17e975206487
jpptm <prncmontano@gmail.com>
2022-05-03 22:55:58 +1000

    Add untested initial templated LinkedList

---
+++ SENG1120_A2/LinkedList.hpp
@@ -0,0 +1,267 @@
+// Implementation
+#include "LinkedList.h"
+
+// Author: Johanne Montano
+// Course: SENG1120
+// Student Number: c3336019
+
+// LinkedList constructor - initialise all pointers to null
+template <typename T>
+LinkedList<T>::LinkedList()
+{
+    head = NULL;
+    current = NULL;
+    tail = NULL;
+}
+
+// LinkedList destructor - destroy all nodes within LL
+template <typename T>
+LinkedList<T>::~LinkedList()
+{
+    // If our LinkedList is not empty go through every single node and delete it
+    Node<T> *temp = head;
+
+    while (temp)
+    {
+        Node<T> *next = temp->getNext();
+        delete temp;
+        temp = next;
+    }
+}
+
+// Add new node as new head of LL
+template <typename T>
+void LinkedList<T>::add_to_head(const T &node_data)
+{
+
+    // Initialise new node in the heap and set node_data as its data
+    Node<T> *new_node = new Node<T>();
+    new_node->set_data(node_data);
+
+    // Make new node as new head and update all pointers
+    if (list_length != 0)
+    {
+        new_node->set_next(head);
+        head->set_previous(new_node);
+
+        new_node->set_previous(NULL);
+
+        head = new_node;
+
+        // Increment list length
+        list_length++;
+    }
+
+    // If list is empty just set that node as head, current and tail
+    else
+    {
+        head = new_node;
+        current = new_node;
+        tail = new_node;
+
+        // Increment list length
+        list_length++;
+    }
+}
+
+// Add new node to the right of the current node - only use if list_length > 2 and current = head
+template <typename T>
+void LinkedList<T>::add_to_current(const T &node_data)
+{
+    // Initialise new node in the heap and set node_data as its data
+    Node<T> *new_node = new Node<T>();
+    new_node->set_data(node_data);
+
+    // If list is empty
+    if (list_length != 0)
+    {
+        // New node's next must be current's next and previous must be current
+        Node<T> *current_next = current->get_next();
+
+        current->set_next(new_node);
+        new_node->set_previous(current);
+
+        new_node->set_next(current_next);
+        current_next->set_previous(new_node);
+
+        current = new_node;
+
+        // Increment list length
+        list_length++;
+    }
+
+    // If list is empty just set that node as head, current and tail
+    else
+    {
+        head = new_node;
+        current = new_node;
+        tail = new_node;
+
+        // Increment list length
+        list_length++;
+    }
+}
+
+template <typename T>
+void LinkedList<T>::add_to_tail(const T &node_data)
+{
+    // Initialise new node in the heap and set node_data as its data
+    Node<T> *new_node = new Node<T>();
+    new_node->set_data(node_data);
+
+    if (list_length != 0)
+    {
+        // Set new node as new tail and update pointers
+        tail->set_next(new_node);
+        new_node->set_previous(tail);
+
+        new_node->set_next(NULL);
+
+        tail = new_node;
+
+        // Increment list length
+        list_length++;
+    }
+
+    // If list is empty just set that node as head, current and tail
+    else
+    {
+        head = new_node;
+        current = new_node;
+        tail = new_node;
+
+        // Increment list length
+        list_length++;
+    }
+}
+
+// Get head node of LL
+template <typename T>
+T &LinkedList<T>::get_from_head()
+{
+    return head;
+}
+
+// Get current node of LL
+template <typename T>
+T &LinkedList<T>::get_from_current()
+{
+    return current;
+}
+
+// Get tail node of LL
+template <typename T>
+T &LinkedList<T>::get_from_tail()
+{
+    return tail;
+}
+
+// Remove head node
+template <typename T>
+T LinkedList<T>::remove_from_head()
+{
+    // If LL is not empty
+    if (list_length != 0)
+    {
+        // Get data within node before deletion and return it
+        T payload = head->get_data();
+
+        // Set head's next as new head, delete head and make sure new head points to null
+        Node<T> *new_head = head->get_next();
+        delete head;
+        head = new_head;
+        head->set_previous(NULL);
+
+        // Decrement list length
+        list_length--;
+
+        return payload;
+    }
+}
+
+// Remove current node - must only use this function if LL has length > 2
+template <typename T>
+T LinkedList<T>::remove_from_current()
+{
+    // If LL is not empty
+    if (list_length != 0)
+    {
+        // Get data within node before deletion and return it
+        T payload = current->get_data();
+
+        // Get current's next and previous nodes and link them together
+        Node<T> *current_next = current->get_next();
+        Node<T> *current_previous = current->get_previous();
+
+        current_previous->set_next(current_next);
+        current_next->set_previous(current_previous);
+
+        // Delete current, set current_previous as new current, return data of deleted node
+        delete current;
+        current = current_previous;
+
+        // Decrement list length
+        list_length--;
+
+        return payload;
+    }
+}
+
+// Remove tail node
+template <typename T>
+T LinkedList<T>::remove_from_tail()
+{
+    // If LL is not empty
+    if (list_length != 0)
+    {
+        // Get tail data before deletion and return it afterwards
+        T payload = tail->get_data();
+
+        // Get tail's previous, set that as new tail, delete current tail, make sure new tail points to NULL, return data
+        Node<T> *new_tail = tail->get_previous();
+
+        delete tail;
+        tail = new_tail;
+        tail->set_next(NULL);
+
+        // Decrement list length
+        list_length--;
+
+        return payload;
+    }
+}
+
+// Set current back to head
+template <typename T>
+void LinkedList<T>::start()
+{
+    current = head;
+}
+
+// Set current to tail
+template <typename T>
+void LinkedList<T>::end()
+{
+    current = tail;
+}
+
+// Move current one node forward
+template <typename T>
+void LinkedList<T>::forward()
+{
+    current = current->get_next();
+}
+
+// Move current one node back
+template <typename T>
+void LinkedList<T>::back()
+{
+    current = current->get_previous();
+}
+
+// Get list's size by returning list_length
+template <typename T>
+int LinkedList<T>::size()
+{
+    return list_length;
+}
