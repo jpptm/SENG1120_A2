5596cf5c50d67dc7a40a96b3596f164cac68c625
jpptm <99300632+jpptm@users.noreply.github.com>
2022-05-09 19:36:29 +1000

    Implement Node class (header and implementation) using template (#1)
    * Implement Node templates (for int only)
    
    * Init (int only)
    
    * Move implementation to Node.hpp and delete cpp
    
    * .
    
    * Add macroguard comment
    
    * Clean code
    
    * .
    
    * Remove useless include cstdlib statement

---
+++ SENG1120_A2/Node.h
@@ -0,0 +1,39 @@
+// Author: Johanne Montano
+// Course: SENG1120
+// Student Number: c3336019
+
+// Macroguard
+#ifndef NODE_H
+#define NODE_H
+
+// This class will serve as data containers for our linked list
+template <class T>
+class Node
+{
+public:
+    // Initialising constructor
+    Node();
+
+    // Initialise empty destructor
+    ~Node();
+
+    // Initialise getters and setters
+    void set_next(Node *next_);
+    void set_previous(Node *previous_);
+    void set_data(const T &node_data_);
+
+    Node *get_next();
+    Node *get_previous();
+    T &get_data();
+
+    // Declare private member variables
+private:
+    Node *next;
+    Node *previous;
+    T node_data;
+};
+
+// Include to let compiler know implementation is in Node.hpp
+#include "Node.hpp"
+
+#endif // NODE_H
---
+++ SENG1120_A2/Node.hpp
@@ -0,0 +1,58 @@
+// Implementation for Node.h is written here
+#include "Node.h"
+
+// Author: Johanne Montano
+// Course: SENG1120
+// Student Number: c3336019
+
+// This class will be used as data holders for a linked list
+
+// Constructors
+template <typename T>
+Node<T>::Node()
+{
+    next = NULL;
+    previous = NULL;
+}
+
+// Empty destructor
+template <typename T>
+Node<T>::~Node() {}
+
+// Setters
+template <typename T>
+void Node<T>::set_next(Node *next_)
+{
+    next = next_;
+}
+
+template <typename T>
+void Node<T>::set_previous(Node *previous_)
+{
+    previous = previous_;
+}
+
+template <typename T>
+void Node<T>::set_data(const T &node_data_)
+{
+    node_data = node_data_;
+}
+
+// Getters - use const when we can and non const when we must alter the state of the object
+template <typename T>
+Node<T> *Node<T>::get_next()
+{
+    return next;
+}
+
+template <typename T>
+Node<T> *Node<T>::get_previous()
+{
+    return previous;
+}
+
+template <typename T>
+T &Node<T>::get_data()
+{
+    return node_data;
+}
